bf2921641979b58dee2e7f6fb8e84569c1d4dbed
dagma <dagmara.ziolkowska@wp.pl>
2019-02-02 20:02:54 +0100

    usunięcie jednej tablicy

--- src/MirrorTab.java
+++ src/MirrorTab.java
@@ -1,25 +1,25 @@
-import java.util.Arrays;
 import java.util.Random;
 
 public class MirrorTab {
-    private final static int maxRand = 100;
+    private final static int maxRand = 10;
     private final static int N = 10;
 
     public static void main(String[] args) {
         Random rand = new Random();
         int[] tab = new int[N];
-        int[] tabMirror = new int[2 * N];
 
         for (int i = 0; i < tab.length; i++) {
             tab[i] = rand.nextInt(maxRand);
         }
 
-        for (int i = 0; i < tab.length; i++) {
-            tabMirror[i] = tab[i];
-            tabMirror[tabMirror.length - 1 - i] = tab[i];
+        for (int i = 0; i < tab.length*2; i++) {
+            if (i<tab.length){
+                System.out.print(tab[i]+", ");
+            } else if (i < tab.length*2 - 1){
+                System.out.print(tab[2*N-i-1]+", ");
+            } else
+                System.out.print(tab[2*N-i-1]);
         }
-//        System.out.println(Arrays.toString(tab));
-        System.out.println(Arrays.toString(tabMirror));
     }
 
 }
